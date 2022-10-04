a1d76ae7b0af8ec9cc6404de3380335d954a7a21
whansoo <ghkstn7523@naver.com>
2022-10-04 23:11:10 +0900

    mongoose로 서버와 데이터베이스 연결

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
 
 const api = require('./api');
 
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+.connect(MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(e => {
+    console.error(e);
+});
 const app = new Koa();
 const router = new Router();
 
@@ -11,7 +24,7 @@ router.use('/api', api.routes());
 app.use (bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
-
+const port = PORT || 4000;
 app.listen(4000, () => {
-    console.log('Listening to port 4000');
+    console.log('Listening to port %d', port);
 })
