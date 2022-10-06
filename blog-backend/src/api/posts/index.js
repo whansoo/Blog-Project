b60d96cff2f578f15ca2a6d6f947de69a68c68a7
whansoo <ghkstn7523@naver.com>
2022-10-06 12:58:55 +0900

    REST API

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -2,14 +2,10 @@ import Router from 'koa-router';
 import * as postsCtrl from './post.ctrl';
 const posts = new Router();
 
-
-
-
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+posts.get('/:id', postsCtrl.checkObjectId,postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId,postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId,postsCtrl.update);
 
 export default posts;
--- blog-backend/src/api/posts/post.ctrl.js
+++ blog-backend/src/api/posts/post.ctrl.js
@@ -1,23 +1,110 @@
-export const write = ctx => {
+import Post from "../../models/post";
+import mongoose from "mongoose";
+import Joi from "joi";
 
+const { ObjectId } = mongoose.Types;
+
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+    return next();
 };
 
-export const list = ctx => {
-
+export const write = async ctx => {
+    const schema = Joi.object().keys({
+      // 객체가 다음 필드를 가지고 있음을 검증
+      title: Joi.string().required(), // required() 가 있으면 필수 항목
+      body: Joi.string().required(),
+      tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
+    });
+  
+    // 검증 후, 검증 실패시 에러처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+      ctx.status = 400; // Bad Request
+      ctx.body = result.error;
+      return;
+    }
+  
+  
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+      title,
+      body,
+      tags,
+      user:ctx.state.user,
+    });
+    try {
+      await post.save();
+      ctx.body = post;
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+  };
+export const list = async ctx => {
+   try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+   } catch (e) {
+    ctx.throw(500, e);
+   }
 };
 
-export const read = ctx => {
-
+export const read = async ctx => {
+   const { id } = ctx.params;
+   try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+        ctx.status = 404;
+        return;
+    }
+    ctx.body = post;
+   } catch (e) {
+    ctx.throw(500, e);
+   }
 };
 
-export const remove = ctx => {
-
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
-export const replace = ctx => {
 
-};
+export const update = async ctx => {
+  const { id } = ctx.params;
 
-export const update = ctx => {
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
 
+  const result = schema.validate(ctx.request.body);
+  if(result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+        new: true,
+    }).exec();
+    if (!post) {
+        ctx.status = 404;
+        return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
